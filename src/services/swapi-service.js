dbe3a529feed6e38438654e0f407d7d19f50dbe1
mr_irmag <mrirmag.pr@gmail.com>
2025-03-15 00:21:47 +0500

    Step 11: unify item details
    Removed `person-details.js` and `person-details.css` as their functionality was integrated into the general `ItemDetails` component. Introduced a universal `ItemDetails` component with dynamic record support via the `Record` component, enabling it to display information for various entities like characters, planets, and starships. Added methods to `SwapiService` for fetching images of characters, planets, and starships (`getPersonImage`, `getStarshipImage`, `getPlanetImage`) to simplify working with visual data. Updated `PeoplePage` to use the new `ItemDetails` component with its universal properties, making the item selection logic more flexible and adaptable to different data types. Removed character-specific styles and logic, replacing them with universal solutions.

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,5 +1,6 @@
 export default class SwapiService {
   _apiBase = "https://swapi.dev/api";
+  _imageBase = "https://starwars-visualguide.com/assets/img";
 
   getResource = async (url) => {
     const res = await fetch(`${this._apiBase}${url}`);
@@ -8,6 +9,18 @@ export default class SwapiService {
       throw new Error(`Could not fetch ${url}` + `, received ${res.status}`);
     }
     return await res.json();
+  };
+
+  getPersonImage = ({ id }) => {
+    return `${this._imageBase}/characters/${id}.jpg`;
+  };
+
+  getStarshipImage = ({ id }) => {
+    return `${this._imageBase}/starships/${id}.jpg`;
+  };
+
+  getPlanetImage = ({ id }) => {
+    return `${this._imageBase}/planets/${id}.jpg`;
   };
 
   getAllPeople = async () => {
