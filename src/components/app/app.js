701957e038c387445db1ce9144b02ba93997d6b4
mr_irmag <mrirmag.pr@gmail.com>
2025-03-15 00:54:55 +0500

    Step 13: Refactor context and HOC
    Refactored the codebase to use React Context (`SwapiServiceContext`) for dependency injection of SwapiService methods. Introduced `PersonDetails`, `PlanetDetails`, and `StarshipDetails` components in `sw-components/details.js`, leveraging context for data fetching and rendering. Consolidated `PersonList`, `PlanetList`, and `StarshipList` into `item-lists.js` using the `withData` HOC for consistent data handling. Simplified `App.js` by replacing hardcoded components with reusable list and detail components. Updated exports in `index.js` for better organization.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-
 import Header from "../header";
 import RandomPlanet from "../random-planet";
-import ItemList from "../item-list";
-import PersonDetails from "../person-details";
-
-import "./app.css";
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
 import SwapiService from "../../services/swapi-service";
-
+import { PersonList, PlanetList, StarshipList } from "../sw-components";
+import ErrorBoundary from "../error-boundary";
+import "./app.css";
 export default class App extends Component {
   swapiService = new SwapiService();
 
@@ -28,11 +25,18 @@ export default class App extends Component {
     }
 
     return (
-      <div>
-        <Header />
-        <RandomPlanet />
-        <PeoplePage />
-      </div>
+      <ErrorBoundary>
+        <div className="stardb-app">
+          <Header />
+          <RandomPlanet />
+
+          <PersonList>{(i) => `${i.name} (${i.birthYear})`}</PersonList>
+
+          <StarshipList>{(i) => `${i.name} (${i.cargoCapacity})`}</StarshipList>
+
+          <PlanetList>{(i) => `${i.name} (${i.population})`}</PlanetList>
+        </div>
+      </ErrorBoundary>
     );
   }
 }
